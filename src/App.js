92518ef894429f55685493db992e02fbeaa6d743
paul <paulnyoiken@gmail.com>
2023-09-22 17:08:19 +0300

    ✨feat: added terms and conditions component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './components/Home/Home';
 import Process from './components/Process/Process';
 import Pricing from './components/Pricing/Pricing';
 import Order from './components/Order/Order';
+import Terms from './components/Terms/Terms';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/home/weddings" element={<Process />} />
           <Route path="/home/studio" element={<Pricing />} />
           <Route path="/order" element={<Order />} />
+          <Route path="/t&c" element={<Terms />} />
         </Routes>
     </Router>
   )
---
+++ src/components/Terms/Terms.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './Terms.css'
+
+const Terms = () => {
+  return (
+    <>
+    <div className='terms-header'>
+      <h1>Terms and Conditions:</h1>
+    </div>
+    <div className='terms-details'>
+      <h1>Payment:</h1>
+      <ol>
+      <li>No cake order is secure or confirmed without a 50% deposit of the total invoiced amount.</li>
+      <li>Payment is only accepted via direct deposit or via the online store for pre-designed cakes</li>
+      <li>Quotes are valid for 2 weeks. If in two weeks you have not notified Cakes By Tabbz of your decision, your quote will expire</li>
+      </ol>
+      <h1>Cancelations:</h1>
+      <ol>
+      <li>Any cancellations made prior to a 50% deposit will not incur a fee.</li>
+      <li>All cancellations made after the 50% deposit and 60 days before your event will not receive a refund for the deposit.</li>
+      <li>All cancellations made after the 50% deposit are subject to a $100 administration fee</li>
+      <li>All cancellations made with less than 7 days notice will forfeit the total cost of the cake.</li>
+      <li>All Postponements must be made at least 3 weeks in advance. A postponement will only be made if the 50% deposit has been received. If the postponement cannot go ahead, the deposit will not be refunded. Postponements are valid for 1 year following the postponement date. If, due to unavailability we cannot accept your postponement date, the cancellation will be subject to our ordinary terms and conditions.</li>
+      <li>In the event that the business must cancel an order, you will be refunded your payment amount.</li>
+      <li>Any cancellations that must be made due to an online order being placed with insufficient notice (Less than 2 weeks) will be charged a $25 admin fee.</li>
+      </ol>
+      <h1>Refunds:</h1>
+      <ol>
+      <li>Cakes By Tabbz is not liable for any damage caused to the cake or products after delivery and set up.</li>
+      <li>Refunds will only be given for damage caused by Cakes By Tabbz.</li>
+      <li>Cakes By Tabbz is not liable for any allergic reactions caused by the ingredients in our cakes. All cakes may contain common allergens such as gluten, nuts, egg and dairy.</li>
+      </ol>
+      <h1>Cake Design</h1>
+      <ol>
+      <li>Orders that have paid a 50% deposit are confirmed cakes and designs. Any alterations to the design may not be accepted. Accepted alterations may incur a price change depending on the changes to the cake design.</li>
+      <li>All designs are unique and may not match exactly with inspiration photos or previous designs.</li>
+      <li>Online cake orders are pre-designed and not subject to changes other than the offered variations.</li>
+      <li>Cakes by Tabbz is not an allergen-friendly kitchen. All products are processed in a kitchen with gluten, eggs and dairy.</li>
+      </ol>
+    </div>
+    </>
+  )
+}
+
+export default Terms
